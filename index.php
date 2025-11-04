05e57d0ce73eb020b1f2714c801727d684de0f43
duonghuupha <duonghuupha@hotmail.com.vn>
2025-11-04 18:11:40 +0700

    24

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+// ✅ Bật CORS toàn cục
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
 // Bật hiển thị lỗi (debug)
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
